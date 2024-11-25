2da5d8297054a3b837acbc55cdbd36278af54bb1
222212602 <222212602@stis.ac.id>
2024-11-25 18:54:08 +0700

    Finish: Asynchronous

--- 2024-11-25.js
+++ 2024-11-25.js
@@ -138,23 +138,63 @@ const delayedColorChange = (color, delay) =>{
     })
 }
 
-delayedColorChange("red", 1000)
-    .then(()=>delayedColorChange("green", 1000))
-    .then(()=>delayedColorChange("yellow", 1000))
-    .then(()=>delayedColorChange("purple", 1000))
-    .then(()=>delayedColorChange("blue", 1000))
-    .then(()=>delayedColorChange("orange", 1000))
-    .then(()=>delayedColorChange("pink", 1000))
-    .then(()=>delayedColorChange("cyan", 1000))
-    .then(()=>delayedColorChange("black", 1000))
+// delayedColorChange("red", 1000)
+//     .then(()=>delayedColorChange("green", 1000))
+//     .then(()=>delayedColorChange("yellow", 1000))
+//     .then(()=>delayedColorChange("purple", 1000))
+//     .then(()=>delayedColorChange("blue", 1000))
+//     .then(()=>delayedColorChange("orange", 1000))
+//     .then(()=>delayedColorChange("pink", 1000))
+//     .then(()=>delayedColorChange("cyan", 1000))
+//     .then(()=>delayedColorChange("black", 1000))
 
-setInterval(()=>{
-    setTimeout(()=>document.body.style.backgroundColor="pink", 500);
-    setTimeout(()=>document.body.style.backgroundColor="green", 1000);
-    setTimeout(()=>document.body.style.backgroundColor="purple", 1500);
-    setTimeout(()=>document.body.style.backgroundColor="red", 2000);
-    setTimeout(()=>document.body.style.backgroundColor="cyan", 2500);
-    setTimeout(()=>document.body.style.backgroundColor="yellow", 3000);
-    setTimeout(()=>document.body.style.backgroundColor="orange", 3500);
-    document.body.style.backgroundColor = "blue"
-}, 4000)
+// setInterval(()=>{
+//     setTimeout(()=>document.body.style.backgroundColor="pink", 500);
+//     setTimeout(()=>document.body.style.backgroundColor="green", 1000);
+//     setTimeout(()=>document.body.style.backgroundColor="purple", 1500);
+//     setTimeout(()=>document.body.style.backgroundColor="red", 2000);
+//     setTimeout(()=>document.body.style.backgroundColor="cyan", 2500);
+//     setTimeout(()=>document.body.style.backgroundColor="yellow", 3000);
+//     setTimeout(()=>document.body.style.backgroundColor="orange", 3500);
+//     document.body.style.backgroundColor = "blue"
+// }, 4000)
+
+const hello = async() => {
+    throw `Maaf ga kenal`;
+    return `Hello World`;
+};
+
+hello()
+    .then((res)=>{
+        console.log(`Response: ${res}`);
+    })
+    .catch((error)=>{
+        console.log(`Error: `, error);
+    })
+
+async function changeColor() {
+    await delayedColorChange("green", 1000);
+    await delayedColorChange("yellow", 1000);
+    await delayedColorChange("purple", 1000);
+    await delayedColorChange("blue", 1000);
+    await delayedColorChange("orange", 1000);
+    await delayedColorChange("pink", 1000);
+    await delayedColorChange("cyan", 1000);
+    await delayedColorChange("black", 1000);
+}
+
+async function printRainbow() {
+    await changeColor();
+    setTimeout(()=>alert(`All Done!`),1000)
+}
+
+printRainbow()
+
+const requestHandling = async ()=>{
+    try{
+        let result = await requestPromise("google.com");
+        console.log(result);
+    } catch (error){
+        console.log(error);
+    }
+}
